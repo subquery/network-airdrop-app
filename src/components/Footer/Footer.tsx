ece85fe48c18a76f8a2b465b15dfaf4c71ea74a0
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-08-19 11:30:01 +1200

    add unneeded css and icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,37 +6,37 @@ import { IconLinks } from "./components";
 import styles from "./style.module.css";
 
 export const Footer: React.FC = () => (
-    <div className={styles.footerWrapper}>
-      <div className={styles.footer}>
-        <div className={styles.primaryRow}>
-          <div className={styles.contact}>
-            <h2>Join the Open Web3 Data Revolution</h2>
-            <IconLinks />
-          </div>
-
-          <div className={styles.discord}>
-            <Button
-              type="primary"
-              ghost
-              shape="round"
-              href="https://discord.com/invite/subquery"
-              size="large"
-            >
-              <div className={styles.discordButton}>
-                <img src={Discord} alt='' className={styles.discordButtonIcon} />
-                <div>Join our active Discord community</div>
-              </div>
-            </Button>
-          </div>
+  <div className={styles.footerWrapper}>
+    <div className={styles.footer}>
+      <div className={styles.primaryRow}>
+        <div className={styles.contact}>
+          <h2>Join the Open Web3 Data Revolution</h2>
+          <IconLinks />
         </div>
-        <div className={styles.secondaryRow}>
-          <div className={styles.links}>
-            <a href="https://subquery.network/privacy">Privacy Policy</a>
-          </div>
-          <div className={styles.copyright}>
-            <small>SubQuery Foundation © 2022</small>
-          </div>
+
+        <div className={styles.discord}>
+          <Button
+            type="primary"
+            ghost
+            shape="round"
+            href="https://discord.com/invite/subquery"
+            size="large"
+          >
+            <div className={styles.discordButton}>
+              <img src={Discord} alt='' className={styles.discordButtonIcon} />
+              <div>Join our active Discord community</div>
+            </div>
+          </Button>
+        </div>
+      </div>
+      <div className={styles.secondaryRow}>
+        <div className={styles.links}>
+          <a href="https://subquery.network/privacy">Privacy Policy</a>
+        </div>
+        <div className={styles.copyright}>
+          <small>SubQuery Foundation © 2022</small>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
--- src/components/Footer/components/IconLinks/IconLinks.tsx
+++ src/components/Footer/components/IconLinks/IconLinks.tsx
@@ -1,57 +1,22 @@
 /* eslint-disable camelcase */
 import React from 'react'
-// TODO: find a more programatic way to do this...
-import { Popover } from 'antd'
 
+// TODO: find a more programatic way to do this...
 import icon_1 from './icons/icon_1.svg'
-import icon_2 from './icons/icon_2.svg'
 import icon_3 from './icons/icon_3.svg'
 import icon_4 from './icons/icon_4.svg'
 import icon_5 from './icons/icon_5.svg'
 import icon_6 from './icons/icon_6.svg'
 import icon_7 from './icons/icon_7.svg'
 import icon_8 from './icons/icon_8.svg'
-import qrcode from './images/qrcode.png'
 import styles from './style.module.css'
 
-export const IconLinks: React.FC = () => {
-  // const icons = []
-  // for (let i = 1; i < 8; i++) {
-    // icons.push('./icons/icon_' + i + '.svg')
-  // }
-
-  // for the weChat Popover
-  const content = (
-    <div className={styles.qrcode}>
-      <img src={qrcode} alt="qrcode" />
-      <p>Scan the QR code to join us</p>
-    </div>
-  )
-
-  return (
+export const IconLinks: React.FC = () => (
     <div className={styles.icons}>
-    {/* why isnt this img working? */}
-    {/* <img src='./icons/icon_1.svg' alt='test' /> */}
-      {/* {icons.map((icon, index) => (
-        <img
-          key={index}
-          src={icon}
-          // alt={'An icon link to a Subquery social media platform'}
-          alt={icon}
-          className={'icon'}
-        />
-      ))} */}
-
-      {/* MAIL */}
+       {/* MAIL */}
       <a href='mailto:hello@subquery.network' target='_blank' rel="noreferrer">
         <img src={icon_1} alt='A social media icon' className={styles.icon} />
       </a>
-      {/* WECHAT */}
-      <Popover
-        content={content}
-        >
-        <img src={icon_2} alt='A social media icon' className={styles.icon} />
-      </Popover>
       {/* TWITTER */}
       <a href='https://twitter.com/subquerynetwork' target='_blank' rel="noreferrer">
         <img src={icon_3} alt='A social media icon' className={styles.icon} />
@@ -79,4 +44,3 @@ export const IconLinks: React.FC = () => {
       </a>
     </div>
   )
-}
