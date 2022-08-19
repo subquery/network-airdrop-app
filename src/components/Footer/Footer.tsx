b60d05c314b02b73d7d50ea1b557bda4e7dbcdcf
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-08-19 12:26:10 +1200

    add intl for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { Button } from "antd";
 
@@ -6,39 +7,43 @@ import Discord from "./images/discord.svg";
 import { IconLinks } from "./components";
 import styles from "./style.module.css";
 
-export const Footer: React.FC = () => (
-  <div className={styles.footerWrapper}>
-    <div className={styles.footer}>
-      <div className={styles.primaryRow}>
-        <div className={styles.contact}>
-          <h2>Join the Open Web3 Data Revolution</h2>
-          <IconLinks />
-        </div>
+export const Footer: React.FC = () => {
+  const {t} = useTranslation();
 
-        <div className={styles.discord}>
-          <Button
-            type="primary"
-            ghost
-            shape="round"
-            href="https://discord.com/invite/subquery"
-            size="large"
-          >
-            <div className={styles.discordButton}>
-              <img src={Discord} alt='discord' className={styles.discordButtonIcon} />
-              <div>Join our active Discord community</div>
-            </div>
-          </Button>
+  return (
+    <div className={styles.footerWrapper}>
+      <div className={styles.footer}>
+        <div className={styles.primaryRow}>
+          <div className={styles.contact}>
+            <h2>Join the Open Web3 Data Revolution</h2>
+            <IconLinks />
+          </div>
+
+          <div className={styles.discord}>
+            <Button
+              type="primary"
+              ghost
+              shape="round"
+              href="https://discord.com/invite/subquery"
+              size="large"
+            >
+              <div className={styles.discordButton}>
+                <img src={Discord} alt='discord' className={styles.discordButtonIcon} />
+                <div>{t(`footer.discord`)}</div>
+              </div>
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className={styles.secondaryRow}>
-        <div className={styles.links}>
-          <a href="https://subquery.network/privacy">Privacy Policy</a>
-          <Link to="/terms-and-conditions">Airdrop Terms & Conditions</Link>
-        </div>
-        <div className={styles.copyright}>
-          <small>SubQuery Foundation © 2022</small>
+        <div className={styles.secondaryRow}>
+          <div className={styles.links}>
+            <a href="https://subquery.network/privacy">{t(`footer.privacy`)}</a>
+            <Link to="/terms-and-conditions">{t(`footer.termsAndConditions`)}</Link>
+          </div>
+          <div className={styles.copyright}>
+            <small>{t(`footer.copyright`)}</small>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
