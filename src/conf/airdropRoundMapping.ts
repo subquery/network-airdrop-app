6305dc3783001eabcb7bd36a0d45dcba0247032c
cyrbuzz <cyrbuzz@foxmail.com>
2024-02-21 16:12:52 +0800

    feat: rounding msg

--- src/conf/airdropRoundMapping.ts
+++ src/conf/airdropRoundMapping.ts
@@ -2,5 +2,20 @@ export const airdropRoundMapping: {
   [key: string]: string;
 } = {
   '0': 'Public Sale Claim',
-
+  // not confirm fields.
+  '1': 'Ambassador Rewards',
+  '2': 'Avalanche Airdrop Campaign',
+  '3': 'Community Call Giveaways',
+  '4': 'Community Events',
+  '5': 'Development Contributions',
+  '6': 'Grant Recipients',
+  '7': 'Hackathons',
+  '8': 'KOL Promotion',
+  '9': 'Launchpad Testing',
+  '10': 'Miscellaneous Giveaways',
+  '11': 'SubQuery Birthday Airdrop Campaign',
+  '12': 'SubQuery Juno Airdrop Campaign',
+  '13': 'SubQuery’s 50k Promo',
+  '14': 'Token Sale 2022 Campaign',
+  '15': 'Zealy Rewards'
 }
